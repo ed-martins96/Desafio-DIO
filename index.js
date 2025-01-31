902b01ece368dbe66444f770e36812928b2d8734
Eder Martins <eder.martins_96@hotmail.com>
2025-01-30 21:30:32 -0300

    Update index.js
    Codigo finalizado .

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-let heroi = ["Geralt D' Rívia", "Gênero: Masculino", "Lvl: 99", "HP: 101"]
-let xp = "10001"
-let patente = ""
+let nomeHeroi = "Goku"
+let xp = 10001
+let formaSaiyajin = ""
 
-if(xp){
-    patente = ""
+if(xp <= 1000){
+    formaSaiyajin = "Kaioken"
+}else if(xp >= 1001 && xp <= 2000 ){
+    formaSaiyajin = "Kaioken x3"
+}else if(xp >= 2001 && xp <= 5000 ){
+    formaSaiyajin = "Kaioken x20"
+}else if(xp >= 5001 && xp <= 7000 ){
+    formaSaiyajin = "Super Saiyajin"
+}else if(xp >= 7001 && xp <= 8000 ){
+    formaSaiyajin = "Super Saiyajin Nível 2"
+}else if(xp >= 8001 && xp <= 9000 ){
+    formaSaiyajin = "Super Saiyajin Nível 3"
+}else if(xp >= 9001 && xp <= 10000 ){
+    formaSaiyajin = "Deus Super Saiyajin"
+}else{
+    formaSaiyajin = "Deus Super Saiyajin Blue"
 }
+
+console.log("O guerreiro Goku está no nível: " + formaSaiyajin)
