d76d531d3d6562a60bf0193320e3f3ae377885e4
Eder Martins <eder.martins_96@hotmail.com>
2025-01-31 21:50:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ let formaSaiyajin = "Deus Super Saiyajin Super Saiyajin"
 
 switch(formaSaiyajin){
     case "Kaioken":
-        console.log("PDL 10000")
+        console.log("PDL 10.000")
         break
     case "Kaioken x3":
-        console.log("PDL 35000")
+        console.log("PDL 35.000")
         break
     case "Kaioken x20":
-        console.log("PDL 180000")
+        console.log("PDL 180.000")
         break
     case "Super Saiyajin":
-        console.log("PDL 1500000")
+        console.log("PDL 150.000.000")
         break
     case "Super Saiyajin 2":
-        console.log("PDL 300000")
+        console.log("PDL 300.000.000")
         break
     case "Super Saiyajin 3":
-        console.log("PDL 12000000")
+        console.log("PDL 1.200.000.000.000")
         break
     case "Deus Super Saiyajin":
-        console.log("PDL 50000000")
+        console.log("PDL 500.000.000.000.000")
         break
     default:
-    console.log("PDL 10000001")
+    console.log("PDL 100.000.000.000.000.001")
 }
 
 console.log("Goku está no nível: " + formaSaiyajin)
